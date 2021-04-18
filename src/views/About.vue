00439d48220000605b4eb351b75b39578d1d3185
nandazman <nandazmann@gmail.com>
2021-04-18 15:19:54 +0700

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,52 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <p class="error">{{ error }}</p>
+
+    <p class="decode-result">Last result: <b>{{ result }}</b></p>
+
+    <qrcode-stream @decode="onDecode" @init="onInit" />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { QrcodeStream } from 'vue-qrcode-reader'; // @ is an alias to /src
+
+@Component({
+  components: {
+    QrcodeStream,
+  },
+})
+
+export default class Home extends Vue {
+  public result = '';
+
+  public error = '';
+
+  onDecode(result: string): void {
+    this.result = result;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  async onInit(promise: never) {
+    try {
+      await promise;
+    } catch (error) {
+      if (error.name === 'NotAllowedError') {
+        this.error = 'ERROR: you need to grant camera access permisson';
+      } else if (error.name === 'NotFoundError') {
+        this.error = 'ERROR: no camera on this device';
+      } else if (error.name === 'NotSupportedError') {
+        this.error = 'ERROR: secure context required (HTTPS, localhost)';
+      } else if (error.name === 'NotReadableError') {
+        this.error = 'ERROR: is the camera already in use?';
+      } else if (error.name === 'OverconstrainedError') {
+        this.error = 'ERROR: installed cameras are not suitable';
+      } else if (error.name === 'StreamApiNotSupportedError') {
+        this.error = 'ERROR: Stream API is not supported in this browser';
+      }
+    }
+  }
+}
+</script>
